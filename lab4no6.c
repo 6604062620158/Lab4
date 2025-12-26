df5f2a1899dbd544488189333d8a36bf8d95e464
Namchai Hangkasee <s6604062620158@email.kmutnb.ac.th>
2025-12-26 22:55:18 +0700

    Commit 3>> lab4no6.c display square of a character

---
+++ lab4no6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	char c;
+	int n ;
+	
+	scanf(" %c %d",&c,&n);
+	for(int i=1; i<=n;i++){
+		printf("\n");
+		for(int j=1; j<=n; j++){
+			
+			printf("%c",c);
+			
+		}
+	}
+
+}
